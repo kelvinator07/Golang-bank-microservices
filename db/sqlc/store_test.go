82e4313c26dddb21d49fa4a3ad028d871c75dff3
Kelvin Isievwore <kelvinator4leo@gmail.com>
2024-01-12 18:27:44 +0100

    Added Store Functionality To Transfer Transactions

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,167 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	testAccount1 := createRandomAccount(t)
+	testAccount2 := createRandomAccount(t)
+	fmt.Println(">> before ", testAccount1.Balance, testAccount2.Balance)
+
+	// run n concurrent transactions
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: testAccount1.ID,
+				ToAccountID:   testAccount2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// check result
+
+	existed := make(map[int]bool)
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		assert.NoError(t, err)
+
+		result := <-results
+		assert.NotEmpty(t, result)
+
+		// check transfer
+		transfer := result.Transfer
+		assert.NotEmpty(t, result)
+		assert.Equal(t, testAccount1.ID, transfer.FromAccountID)
+		assert.Equal(t, testAccount2.ID, transfer.ToAccountID)
+		assert.Equal(t, amount, transfer.Amount)
+		assert.NotZero(t, transfer.ID)
+		assert.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		assert.NoError(t, err)
+
+		// check entries
+		fromEntry := result.FromEntry
+		assert.NotEmpty(t, fromEntry)
+		assert.Equal(t, testAccount1.ID, fromEntry.AccountID)
+		assert.Equal(t, -amount, fromEntry.Amount)
+		assert.NotZero(t, fromEntry.ID)
+		assert.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		assert.NoError(t, err)
+
+		toEntry := result.ToEntry
+		assert.NotEmpty(t, toEntry)
+		assert.Equal(t, testAccount2.ID, toEntry.AccountID)
+		assert.Equal(t, amount, toEntry.Amount)
+		assert.NotZero(t, toEntry.ID)
+		assert.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		assert.NoError(t, err)
+
+		// check accounts
+		fromAccount := result.FromAccount
+		assert.NotEmpty(t, fromAccount)
+		assert.Equal(t, testAccount1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		assert.NotEmpty(t, toAccount)
+		assert.Equal(t, testAccount2.ID, toAccount.ID)
+
+		// check account balance
+		fmt.Println(">> tx ", fromAccount.Balance, toAccount.Balance)
+		diff1 := testAccount1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - testAccount2.Balance
+		assert.Equal(t, diff1, diff2)
+		assert.True(t, diff1 > 0)
+		assert.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount ... n * amount
+
+		k := int(diff1 / amount)
+		assert.True(t, k >= 1 && k <= n)
+		assert.NotContains(t, existed, k)
+		existed[k] = true
+	}
+
+	// check final updated balances
+	updateAccount1, err := testQueries.GetAccount(context.Background(), testAccount1.ID)
+	assert.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), testAccount2.ID)
+	assert.NoError(t, err)
+
+	fmt.Println(">> after ", updateAccount1.Balance, updateAccount2.Balance)
+	assert.Equal(t, testAccount1.Balance-int64(n)*amount, updateAccount1.Balance)
+	assert.Equal(t, testAccount2.Balance+int64(n)*amount, updateAccount2.Balance)
+
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	testAccount1 := createRandomAccount(t)
+	testAccount2 := createRandomAccount(t)
+	fmt.Println(">> before ", testAccount1.Balance, testAccount2.Balance)
+
+	// run n concurrent transactions
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := testAccount1.ID
+		toAccountID := testAccount2.ID
+
+		if i%2 == 1 {
+			fromAccountID = testAccount2.ID
+			toAccountID = testAccount1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+			errs <- err
+		}()
+	}
+
+	// check result
+	for i := 0; i < n; i++ {
+		err := <-errs
+		assert.NoError(t, err)
+	}
+
+	// check final updated balances
+	updateAccount1, err := testQueries.GetAccount(context.Background(), testAccount1.ID)
+	assert.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), testAccount2.ID)
+	assert.NoError(t, err)
+
+	fmt.Println(">> after: ", updateAccount1.Balance, updateAccount2.Balance)
+
+	assert.Equal(t, testAccount1.Balance, updateAccount1.Balance)
+	assert.Equal(t, testAccount2.Balance, updateAccount2.Balance)
+
+}
